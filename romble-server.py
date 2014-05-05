b1664a8f669f981dd014fb3f917a0da38b5a6e6d
mipani <mipani@users.noreply.github.com>
2014-05-04 20:54:17 -0400

    Development of the DB layer

--- romble-server.py
+++ romble-server.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
+
 from flask import Flask
-from romble_db.RombleDBHelper import RombleDBHelper
+from romble_db.RombleDBHelper import RombleDBHelper, Game
+
 app = Flask(__name__)
 dbHelper = RombleDBHelper(app)
+
+@app.before_request
+def setup_db():
+	dbHelper.init_db()
 
 @app.route('/')
 def hello_world():
     return 'This is the very beginning of the Romble project.'
 
+# Used only for very cursory testing!
 @app.route('/test')
 def test():
-	dbHelper.init_db()
-	return dbHelper.test()
+	game = dbHelper.get_game_by_id(1)
+	return str(game.id) + ' ' + game.filename + ' ' + game.title + ' ' + game.description
 
 if __name__ == '__main__':
     app.run(debug=True)
--- romble_db/RombleDBHelper.py
+++ romble_db/RombleDBHelper.py
@@ -1,7 +1,21 @@
+import os
 import sqlite3
 from flask import Flask, g
 
-DATABASE = 'romble-db.db'
+dir = os.path.dirname(__file__)
+DATABASE = os.path.join(dir, 'romble-db.db')
+
+class Game:
+	id = None
+	filename = None
+	title = None
+	description = None
+	
+	def __init__(self, id=None, filename=None, title=None, description=None):
+		self.id = id
+		self.filename = filename
+		self.title = title
+		self.description = description
 
 class RombleDBHelper:
 	db = None
@@ -14,13 +28,19 @@ class RombleDBHelper:
 		self.close_connection = self.appContext.teardown_appcontext(self.close_connection)
 
 	def test(self):
-		print self.db
+		print self.db.cursor()
 		return str(self.db)
 	
 	def init_db(self):
 		self.db = getattr(g, '_database', None)
 		if self.db is None:
 			self.db = g._database = sqlite3.connect(DATABASE)
+	
+	def get_game_by_id(self, id):
+		cursor = self.db.cursor()
+		cursor.execute("SELECT * FROM game WHERE id=?", ( id, ) )
+		row = cursor.fetchone()
+		return Game( row[0], row[1], row[2], row[3] )
 	
 	def close_connection(self, exception):
 		self.db = getattr(g, '_database', None)
